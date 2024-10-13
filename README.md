a6b8361dae4d5303a641a4442172155c5dbf7406
Oscar Serantes Mccormack <oscardjbackup@gmail.com>
2024-10-13 23:10:34 +0000

    fleshes out main.py and updates the readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # tf-chatbot
-Basic chatbot trained on an existing model 
+Basic chatbot trained on an existing model. 
+
+## How can I train the model? 
+
+You can train the model by using the `train_model.py` file. Assuming you are already 
+using poetry that can be done by running: 
+```bash 
+$ poetry run python3 train_model.py
+```
+
+## How can I interact with the model? 
+
+Also assuming you are using poetry, run the ``main.py`` file. This can also be done by running: 
+```bash
+$ poetry run python3 main.py 
+```
+
+## Requirements 
+
+To install this project you need to have a valid poetry installation, and python 3.12, installing the dependencies can
+be done by  running this command: 
+
+```bash
+$ poetry install
+```
--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import json
+from datetime import datetime
+from typing import Any
+
+from model.chatbot_interface import predict_class, generate_response
+
+intents = json.loads(open("~/resources/intents.json").read())
 
 if __name__ == '__main__':
-    print("Temporary")
+    print(f"Bot started running at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
+
+    while True:
+        # Read input from the user
+        user_input: str = input()
+        intentions: list[dict[str, str | Any]] = predict_class(user_input=user_input)
+        # Generate the response and push to user in below print
+        response: str = generate_response(list_of_intents=intentions)
+        print(f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - Bot generated the response of:\n{response}")
